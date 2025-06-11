94438764d7109e1c0fa8e0de2b86beed3a546dd8
Kim Bjerge <au288681@uni.au.dk>
2025-06-11 18:26:45 +0200

    Created installation bash script for Raspberry Pi Python environment

---
+++ raspberryPiEnv.sh
@@ -0,0 +1,8 @@
+python -m venv /home/pi/venv
+source /home/pi/venv/bin/activate
+python -m pip install opencv-python
+python -m pip install torch torchvision torchaudio
+pip install -U scikit-image
+pip install -U scikit-learn
+pip install ultralytics
+
