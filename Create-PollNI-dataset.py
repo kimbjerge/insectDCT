766cc708eedd1dc2a4eec69fc0105ba7e7c53707
Kim Bjerge <au288681@uni.au.dk>
2025-09-16 13:25:20 +0200

    Update Create-PollNI-dataset.py

--- Create-PollNI-dataset.py
+++ Create-PollNI-dataset.py
@@ -18,19 +18,32 @@ from common.motionEnhancement import MotionEnhancement
 IMG_WIDTH = 1920
 IMG_HEIGHT = 1080
 
-def createLabelsAndImages(selDataset, data_df, pathToRecordedFiles, pathToDestDataset, pathToDestDatasetMIE):
+def createLabelsAndImages(cameraSystem, selDataset, data_df, pathToRecordedFiles, pathToDestDataset, pathToDestDatasetMIE, split):
     
     mie = MotionEnhancement()
     
+    skip = int(100/split)
+    count = 0
     for idx, row in selDataset.iterrows():
         detections_df = data_df.loc[data_df['fileName'] == row['fileName']]
-        imageFilePath =  row['fileName'].split('/')[0] + '/'
+        imageFilePath =  row['fileName'].split('/')[0] 
         imageFileName = row['fileName'].split('/')[1]
         labelFileName = imageFileName.replace('.jpg', '.txt')
-        print(labelFileName)
-        labelFile = open(pathToDestDataset+labelFileName, "w")
+        cameraId = "PW_" + cameraSystem + "_" + imageFilePath + '_'
+        imageFilePath += '/'
+        count += 1
+        if count % skip == 0: # Save to test dataset
+            pathToDest = pathToDestDataset.replace("train", "test")
+            pathToDestMIE = pathToDestDatasetMIE.replace("train", "test")
+            print("Test image", cameraId+labelFileName)
+        else: # save to train dataset
+            pathToDest = pathToDestDataset
+            pathToDestMIE = pathToDestDatasetMIE
+            print("Train image", cameraId+labelFileName)
+            
+        labelFile = open(pathToDest+cameraId+labelFileName, "w")
         for i, detection in detections_df.iterrows():
-            print(detection['fileName'], detection['x1'], detection['y1'], detection['x2'], detection['y2'])
+            #print(detection['fileName'], detection['x1'], detection['y1'], detection['x2'], detection['y2'])
             w = detection['x2'] - detection['x1']
             h = detection['y2'] - detection['y1']
             xc = detection['x1'] + 0.5*w
@@ -39,14 +52,18 @@ def createLabelsAndImages(selDataset, data_df, pathToRecordedFiles, pathToDestDa
             print(line)
             labelFile.write(line + "\n")
         labelFile.close()
-        shutil.copyfile(pathToRecordedFiles+row['fileName'], pathToDestDataset+imageFileName)
+        shutil.copyfile(pathToRecordedFiles+row['fileName'], pathToDest+cameraId+imageFileName)
         mieImage = mie.motion_enhanced_image2(pathToRecordedFiles+imageFilePath, imageFileName)
-        cv2.imwrite(pathToDestDatasetMIE+imageFileName, mieImage)
+        cv2.imwrite(pathToDestMIE+cameraId+imageFileName, mieImage)
     
 if __name__=='__main__':
     
-    numImages = 100
-    cameraSystem = "S3"
+    cameraSystem = "S3" # S4, S5
+    numInsects = 600
+    numUnsure = 300
+    numVegetation = 300
+    splitPercentage = 20 # Percentage of image used for test
+    
     pathToSrcDataset = 'J:/PollNI/' + cameraSystem + '/'
     pathToRecordData = 'O:/Tech_TTH-KBE/PollinatorWatch/FIN/' + cameraSystem + '/'
     pathToDestDatasetMIE = 'J:/PollNI/trainPollWm/'
@@ -68,8 +85,16 @@ if __name__=='__main__':
     # Select only images where insects has been detected - many are false positive detections
     selDataset1 = data_frames.loc[data_frames['taxaLabel'] != "Vegetation"]
     selDataset2 = selDataset1.loc[selDataset1['taxaLabel'] != "Unsure"]
-    selDataset3 = selDataset2.sample(n=numImages, random_state=42)
-    createLabelsAndImages(selDataset3, data_frames, pathToRecordData, pathToDestDataset, pathToDestDatasetMIE)
+    selDataset3 = selDataset2.sample(n=numInsects, random_state=37)
+    createLabelsAndImages(cameraSystem, selDataset3, data_frames, pathToRecordData, pathToDestDataset, pathToDestDatasetMIE, splitPercentage)
+    
+    selDataset1 = data_frames.loc[data_frames['taxaLabel'] == "Vegetation"]
+    selDataset2 = selDataset1.sample(n=numUnsure, random_state=65)
+    createLabelsAndImages(cameraSystem, selDataset2, data_frames, pathToRecordData, pathToDestDataset, pathToDestDatasetMIE, splitPercentage)
+    
+    selDataset1 = data_frames.loc[data_frames['taxaLabel'] == "Unsure"]
+    selDataset2 = selDataset1.sample(n=numVegetation, random_state=43)
+    createLabelsAndImages(cameraSystem, selDataset2, data_frames, pathToRecordData, pathToDestDataset, pathToDestDatasetMIE, splitPercentage)
     
     
     
