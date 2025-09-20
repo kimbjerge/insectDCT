f1ddc2c10971f8388932d7d4892c2c54f9fc3ce9
Kim Bjerge <au288681@uni.au.dk>
2025-09-20 09:42:51 +0200

    Update createTrackCrops.py

--- createTrackCrops.py
+++ createTrackCrops.py
@@ -12,64 +12,68 @@ import matplotlib.pyplot as plt
 validTrackLength = 3
 validTrackConfidence = 50
 
-def plotTrackCrops(pathToRecordData, pathToDestCrops, trackDate, trackId, taxa, trackRows):
+def plotTrackCrops(pathToRecordData, pathToDestCrops, trackDate, trackId, taxa, confidence, trackRows):
     
     # Define grid size
-    rows, cols = 5, 5
+    rows, cols = 4, 4
     
     # Create a figure and axes
     fig, axes = plt.subplots(rows, cols, figsize=(20, 20))
+    plt.rcParams.update({'font.size': 20})
     axes = axes.flatten()
 
     i = 0
     for row in trackRows:
 
-        if i >= rows*cols:
-            break
-                
-        imageFilePath = pathToRecordData + str(trackDate) + '/' + row['fileName']
-        
-        image = cv2.imread(imageFilePath)
+        if i < rows*cols:                
+            imageFilePath = pathToRecordData + str(trackDate) + '/' + row['fileName']
+            print(imageFilePath)
+            image = cv2.imread(imageFilePath)
+    
+            height, width, channels = image.shape
+            
+            w = row['width']
+            h = row['height']
+            xc = row['xc']
+            yc = row['yc']
+    
+            if h > w: 
+                wh2 = int(round(h/2))
+            else:
+                wh2 = int(round(w/2))
+            
+            x1 = xc-wh2
+            if x1 < 0:
+               x1 = 0
+            x2 = xc+wh2
+            if x2 >= width:
+                x2 = width-1
+            y1 = yc-wh2
+            if y1 < 0: 
+                y1 = 0
+            y2 = yc+wh2
+            if y2 >= height:
+                y2 = height-1        
+                        
+            # get image crop of object
+            imgCrop = image[y1:y2, x1:x2,  :].copy()
+            if taxa == row['taxaLabel']:
+                color = [0, 0, 255]
+            else:
+                color = [255, 0, 0]
+                plt.title(row['taxaLabel'])
+            for r in range(wh2*2):
+                imgCrop[r, 0, :] = color 
+                imgCrop[r, 1, :] = color 
+            axes[i].imshow(imgCrop)
+            axes[i].axis('off')  # Hide axes
 
-        height, width, channels = image.shape
-        
-        w = row['width']
-        h = row['height']
-        xc = row['xc']
-        yc = row['yc']
-
-        if h > w: 
-            wh2 = int(round(h/2))
-        else:
-            wh2 = int(round(w/2))
-        
-        x1 = xc-wh2
-        if x1 < 0:
-           x1 = 0
-        x2 = xc+wh2
-        if x2 >= width:
-            x2 = width-1
-        y1 = yc-wh2
-        if y1 < 0: 
-            y1 = 0
-        y2 = yc+wh2
-        if y2 >= height:
-            y2 = height-1        
-                    
-        # get image crop of object
-        imgCrop = image[y1:y2, x1:x2,  :].copy()
-        if taxa == row['taxaLabel']:
-            color = [0, 0, 255]
-        else:
-            color = [255, 0, 0]
-        for r in range(wh2*2):
-            imgCrop[r, 0, :] = color 
-            imgCrop[r, 1, :] = color 
-        axes[i].imshow(imgCrop)
-        axes[i].axis('off')  # Hide axes
         i += 1
         
-    plt.tight_layout()
+    plt.suptitle(taxa + " Id " + str(trackId) + " Len " + str(i) + " Conf " + str(confidence) + " Date " + str(trackDate))
+    plt.tight_layout(pad=1.0)
+
+    plt.savefig(pathToDestCrops + taxa + "_" + str(trackId) + "_" + row['fileName'].replace(".jpg", ".png"))
     plt.show()
         
     
@@ -98,7 +102,8 @@ def calcConfidence(trackRows):
             maximum = value
             taxa = key
    
-    confidence = 100*maximum/(counts+1) # Newer version not +1
+    confidence = int(1000*maximum/counts) # Newer version not +1
+    confidence = confidence/10
     
     return confidence, taxa, counts 
     
@@ -110,7 +115,7 @@ def saveTrackCrops(pathToRecordData, pathToDestCrops, trackDate, trackId, trackR
     print(trackDate, trackId, taxa,  counts, f"{confidence:.1f}")
     
     if confidence >= validTrackConfidence:
-        plotTrackCrops(pathToRecordData, pathToDestCrops, trackDate, trackId, taxa, trackRows)
+        plotTrackCrops(pathToRecordData, pathToDestCrops, trackDate, trackId, taxa, confidence, trackRows)
     
 def analyseTracks(data_fp, pathToRecordData, pathToDestCrops):
     
