9ad0bb4df48fbd1b08beeb08b52b9b4006ebe6de
Kim Bjerge <au288681@uni.au.dk>
2025-10-11 15:20:24 +0200

    Updated counting labels to include computing pixel size of boxes

--- countLabels.py
+++ countLabels.py
@@ -6,7 +6,10 @@ Python script to count number of labels
 @author: Kim Bjerge
 """
 import os
+import numpy as np
 
+IMG_WIDTH = 1920
+IMG_HEIGHT = 1080
 
 def countLabels(image_dic, name):
 
@@ -16,6 +19,9 @@ def countLabels(image_dic, name):
         
     backgrounds = 0
     totalImages = 0
+    boxesWidth = []
+    boxesHeight = []
+    boxesArea = []
     for filename in os.listdir(image_dic):
         if (filename.endswith('.txt')) and filename != "classes.txt":
             file = open(image_dic+filename, 'r')
@@ -32,11 +38,23 @@ def countLabels(image_dic, name):
                 subsplit = splitted[line].split(' ')
                 if len(subsplit) == 5: # required: index x y w h
                     index = int(subsplit[0])
+                    xc = float(subsplit[1])*IMG_WIDTH
+                    yc = float(subsplit[2])*IMG_HEIGHT
+                    w = float(subsplit[3])*IMG_WIDTH
+                    h = float(subsplit[4])*IMG_HEIGHT
                     if index < 9:
                        indexCounts[index] +=1
+                    boxesWidth.append(w)
+                    boxesHeight.append(h)
+                    boxesArea.append(w*h)
             
     print(name, indexCounts, ' total:', sum(indexCounts))
     print("Total images:", totalImages, "background:", backgrounds)
+    print("Average labels width:", np.mean(boxesWidth), "STD:", np.std(boxesWidth))
+    print("Average labels height:", np.mean(boxesHeight), "STD:", np.std(boxesHeight))
+    print("Average labels area:", np.mean(boxesArea), "STD:", np.std(boxesArea))
+    
+    return boxesWidth, boxesHeight
     
     
 if __name__=='__main__':
@@ -54,20 +72,43 @@ if __name__=='__main__':
 
     #image_dic = '/home/don/yolov5r/datasets/insects/labels/train1201/'
     #image_dic = '/home/don/yolov5r/datasets/insects/labels/val1201/'
+    boxesWidth = []
+    boxesHeight = []
+    
     image_path = 'D:/insectsDCT_datasets/detectTestDataset/'
     image_dic = image_path + 'testGreenH/'
-    countLabels(image_dic, "Green house Logitech test: ")
+    boxW, boxH = countLabels(image_dic, "Green house Logitech test: ")
+    boxesWidth += boxW
+    boxesHeight += boxH
+    
     image_dic = image_path + 'testHeather/'
-    countLabels(image_dic, "KU Heather Wingscapes test: ")
-    image_dic = image_path + 'testMBO/'
-    countLabels(image_dic, "MAMBO Wingscapes test: ")
-    image_dic = image_path + 'testOrchard/'
-    countLabels(image_dic, "Orchard Pi3Cam test: ")
-    image_dic = image_path + 'testPollW/'
-    countLabels(image_dic, "Pollinator Watch Logitech test: ")
-    image_dic = image_path + 'testRTNI/'
-    countLabels(image_dic, "Real-time Nature Impact Logitech test: ")
+    boxW, boxH = countLabels(image_dic, "KU Heather Wingscapes test: ")
+    boxesWidth += boxW
+    boxesHeight += boxH
 
+    image_dic = image_path + 'testMBO/'
+    boxW, boxH = countLabels(image_dic, "MAMBO Wingscapes test: ")
+    boxesWidth += boxW
+    boxesHeight += boxH
+
+    image_dic = image_path + 'testOrchard/'
+    boxW, boxH = countLabels(image_dic, "Orchard Pi3Cam test: ")
+    boxesWidth += boxW
+    boxesHeight += boxH
+
+    image_dic = image_path + 'testPollW/'
+    boxW, boxH = countLabels(image_dic, "Pollinator Watch Logitech test: ")
+    boxesWidth += boxW
+    boxesHeight += boxH
+
+    image_dic = image_path + 'testRTNI/'
+    boxW, boxH = countLabels(image_dic, "Real-time Nature Impact Logitech test: ")
+    boxesWidth += boxW
+    boxesHeight += boxH
+    
+    print("Average all labels width:", np.mean(boxesWidth), "STD:", np.std(boxesWidth))
+    print("Average all labels height:", np.mean(boxesHeight), "STD:", np.std(boxesHeight))
+ 
     """
     image_dic = 'D:/MAMBO/trainMBO/'
     countLabels(image_dic, "MAMBO train: ")
