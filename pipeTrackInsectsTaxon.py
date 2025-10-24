93ff982f6db4cbbdfa6f9c4f344cad446be30d56
Kim Bjerge <au288681@uni.au.dk>
2025-10-24 20:44:48 +0200

    Update pipeTrackInsectsTaxon.py

--- pipeTrackInsectsTaxon.py
+++ pipeTrackInsectsTaxon.py
@@ -256,7 +256,7 @@ def run(trackName, imagePath, detectPath, trackPath, conf, taxaHierarchy, ignore
                 file_name = imagePath+filepath
                 if videoCap != None:
                     sucess = True
-                    while sucess and (frame_count < insect['frameId']):
+                    while sucess and (frame_count < insect['frameId'] + 1):
                         success, im = videoCap.read()
                         frame_count += 1         
                 else:
