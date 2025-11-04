a64d51e45b4c47304dd5e40abf42c33755d9125f
Kim Bjerge <au288681@uni.au.dk>
2025-11-04 19:49:00 +0100

    Update plot.py

--- hierarchicalB3L/plot.py
+++ hierarchicalB3L/plot.py
@@ -37,7 +37,7 @@ def plot_loss_acc(path, num_epoch, train_accuracies_level1, train_accuracies_lev
     plt.show()
     
     sns.lineplot(data=data_levels, x='Epochs', y='Accuracy', hue='Level', style='Mode')
-    plt.xticks(np.arange(1, 25, 2))
+    plt.xticks(np.arange(1, 30, 2))
     plt.title('Train and validation accuracy')
     plt.savefig(path+f'accuracy_train_validation_epoch.png')
 
@@ -67,7 +67,7 @@ def plot_loss_acc(path, num_epoch, train_accuracies_level1, train_accuracies_lev
     data = pd.concat([train_loss_df, test_loss_df])
 
     sns.lineplot(data=data, x='Epochs', y='Loss', style='Mode')
-    plt.xticks(np.arange(1, 25, 2))
+    plt.xticks(np.arange(1, 30, 2))
     plt.title('Avarge loss of levels L1,L2,L3')
 
     plt.savefig(path+f'loss_epoch.png')
