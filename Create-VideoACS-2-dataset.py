eabd939c7009820f4bbfa7c19b5e7572fb025269
Kim Bjerge <au288681@uni.au.dk>
2026-01-14 10:12:40 +0100

    Added list of contributions

---
+++ Create-VideoACS-2-dataset.py
@@ -0,0 +1,287 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 28 14:38:53 2025
+
+Used to create dataset for detection using YOLO11 based on RGB and MIE images selected from the paper:
+Serra-Martin et. al "Comparative assessment of automated and manual monitoring in comprehensive plant–pollinator communities"
+Use systems with ACS-HQ cameras to create 10 sec. videos each 1 minute
+Paper:https://doi.org/10.1111/2041-210X.70165
+    
+@author: Kim Bjerge
+"""
+
+import os
+import cv2
+import pandas 
+import pandas as pd
+from common.motionEnhancement import MotionEnhancement
+
+# Save an empty frame with offset of last detected insects 
+save_empty_frame_offset = 2 # If 0 then no frames are saved
+#save_empty_frame_offset = 0 # If 0 then no frames are saved
+frame_id_offset = 2 # frame_stride = 1 during detection
+#frame_id_offset = 0 # frame_stride = 3 during detection
+
+# Identification of camera sites
+cameraId = "PP_"
+
+videoNames = [
+    "ACS-HQ108_2024_03_06_12_14_30.mp4",
+    "ACS-HQ122_2024_03_06_12_14_30.mp4",
+    "ACS-HQ15_2024_03_06_12_14_30.mp4",
+    "ACS-HQ16_2024_03_06_12_14_30.mp4",
+    "ACS-HQ17_2024_03_06_12_14_30.mp4",
+    "ACS-HQ200_2024_02_20_19_03_33_Diptera.mp4",
+    "ACS-HQ200_2024_02_21_09_32_34.mp4",
+    "ACS-HQ200_2024_02_21_18_50_20_lasioglossum.mp4",
+    "ACS-HQ200_2024_03_05_08_20_01.mp4",
+    "ACS-HQ200_2024_03_05_10_21_08.mp4",
+    "ACS-HQ200_2024_03_06_12_14_30.mp4",
+    "ACS-HQ200_2024_03_06_18_28_57.mp4",
+    "ACS-HQ200_2024_04_14_05_24_07_microdi.mp4",
+    "ACS-HQ200_2024_04_14_07_01_19.mp4",
+    "ACS-HQ200_2024_04_14_08_25_53.mp4",
+    "ACS-HQ200_2024_04_14_18_15_07.mp4",
+    "ACS-HQ200_2024_04_14_18_27_20.mp4",
+    "ACS-HQ200_2024_04_15_06_08_55.mp4",
+    "ACS-HQ200_2024_04_15_07_29_00.mp4",
+    "ACS-HQ200_2024_05_05_32_00_00_cam1.mp4",
+    "ACS-HQ200_2024_05_14_21_30_26.mp4",
+    "ACS-HQ200_2024_05_15_02_15_15.mp4",
+    "ACS-HQ200_2024_05_22_11_49_58.mp4",
+    "ACS-HQ200_2024_05_22_15_30_37.mp4",
+    "ACS-HQ200_2024_05_24_16_41_42_lasioglossum.mp4",
+    "ACS-HQ201_2025_05_04_10_25_00_cam1.mp4",
+    "ACS-HQ201_2025_05_04_38_00_00_cam1.mp4",
+    "ACS-HQ201_2025_05_04_41_00_00_cam1.mp4",
+    "ACS-HQ201_2025_05_04_47_00_00_cam1.mp4",
+    "ACS-HQ201_2025_05_04_49_00_00_cam1.mp4",
+    "ACS-HQ201_2025_05_04_58_00_00_cam1.mp4",
+    "ACS-HQ201_2025_05_04_77_00_00_cam1.mp4",
+    "ACS-HQ201_2025_05_04_89_00_00_cam1.mp4",
+    "ACS-HQ202_2025_05_05_01_00_00_cam2.mp4",
+    "ACS-HQ202_2025_05_05_11_02_00_cam2.mp4",
+    "ACS-HQ202_2025_05_05_11_08_00_cam2.mp4",
+    "ACS-HQ202_2025_05_05_23_00_00_cam2.mp4",
+    "ACS-HQ202_2025_05_05_32_00_00_cam2.mp4",
+    "ACS-HQ202_2025_05_05_42_00_00_cam2.mp4",
+    "ACS-HQ202_2025_05_05_45_00_00_cam2.mp4",
+    "ACS-HQ202_2025_05_05_80_00_00_cam2.mp4",
+    "ACS-HQ28_2024_03_06_12_14_30.mp4",
+    "ACS-HQ30_2024_03_06_12_14_30.mp4",
+    "ACS-HQ39_2024_03_06_12_14_30.mp4",
+    "ACS-HQ45_2024_03_06_12_14_30.mp4",
+    "ACS-HQ46_2024_03_06_12_14_30.mp4",
+    "ACS-HQ4_2024_03_06_12_14_30.mp4",
+    "ACS-HQ58_2024_03_06_12_14_30.mp4",
+    "ACS-HQ61_2024_03_06_12_14_30.mp4",
+    "ACS-HQ69_2024_03_06_12_14_30.mp4",
+    "ACS-HQ70_2024_03_06_12_14_30.mp4",
+    "ACS-HQ71_2024_03_06_12_14_30.mp4",
+    "ACS-HQ83_2024_03_06_12_14_30.mp4"
+    ]
+
+# UFZ image size, Pi model 3 camera HD resolution
+IMG_WIDTH = 1920
+IMG_HEIGHT = 1080
+
+def saveImages(frameId, srcFilename, count, skip, imageRGB, imageMIE, text="Insect"):
+    
+    frameIdTxt = '_' + str(frameId) + '.jpg'
+    fileId = videoNames.index(srcFilename)
+    #imageFileName = srcFilename.replace('.mp4', frameIdTxt)
+    imageFileName = 'S' + str(fileId) + frameIdTxt
+    labelFileName = imageFileName.replace('.jpg', '.txt')
+
+    count += 1
+    if count % skip == 0: # Save to test dataset
+        # pathToDest = pathToDestDataset.replace("train", "val")
+        # pathToDestMIE = pathToDestDatasetMIE.replace("train", "val")
+        pathToDest = pathToDestDataset.replace("train", "test")
+        pathToDestMIE = pathToDestDatasetMIE.replace("train", "test")
+        print(text + " Val   image", cameraId+labelFileName)
+    else: # save to train dataset
+        pathToDest = pathToDestDataset
+        pathToDestMIE = pathToDestDatasetMIE
+        print(text + " Train image", cameraId+labelFileName)
+    
+    cv2.imwrite(pathToDest+cameraId+imageFileName, imageRGB)
+    cv2.imwrite(pathToDestMIE+cameraId+imageFileName, imageMIE)
+    
+    return count, labelFileName, pathToDest
+            
+def createLabelsAndImages(selDataset, data_df, pathToRecordedFiles, pathToDestDataset, pathToDestDatasetMIE, split):
+    
+    mie = MotionEnhancement()
+    
+    skip = int(100/split)
+    count = 0
+    frame_count = 0
+    frameId = 0
+    currentVideoFileName = ""
+    videoCap = None
+    imageRGB = None
+    imageNext = None
+
+    for idx, row in selDataset.iterrows():
+        
+        if currentVideoFileName != row['fileName']: # Check for new video
+            currentVideoFileName = row['fileName']
+            pathToVideoFile = pathToRecordedFiles + currentVideoFileName
+            print("Uses video recording", pathToVideoFile)
+            if videoCap != None:
+                videoCap.release()
+            videoCap = cv2.VideoCapture(pathToVideoFile) # Opens new video file to capture frames
+            frame_count = 0
+        
+        frameIdLast = frameId
+        frameId = row['frameId']
+
+        print("Video frame", frame_count, "detected frame", frameId)
+        
+        # Handle saving empty image with save_empty_frame_offset of last frame
+        if (frameId > frameIdLast + save_empty_frame_offset) and (save_empty_frame_offset > 0) and (frameIdLast > 0):
+            framePosEmpty = frameIdLast + save_empty_frame_offset + frame_id_offset
+            success = True
+            while success and (frame_count < framePosEmpty): # Reading next image to create MIE
+                imageRGB = imageNext
+                success, imageNext = videoCap.read()
+                if success and (frame_count > framePosEmpty - 3):
+                    imageMIE, imgPrev = mie.motion_image(imageNext)
+                frame_count += 1
+                
+            if success:
+                count, labelFileName, pathToDest = saveImages(frameIdLast + save_empty_frame_offset, 
+                                         row['fileName'], count, skip, imageRGB, imageMIE, text="Empty ")
+                
+                labelFile = open(pathToDest+cameraId+labelFileName, "w") # Create empty label file
+                labelFile.close()
+                
+        # Handle saving image with detections
+        framePos = frameId + frame_id_offset
+        success = True
+        if frameId < 3: # Ignore first frames 
+            success = False
+        while success and (frame_count < framePos): # Reading next image to create MIE
+            imageRGB = imageNext
+            success, imageNext = videoCap.read()
+            if success and (frame_count > framePos - 3):
+                imageMIE, imgPrev = mie.motion_image(imageNext)
+            frame_count += 1
+        
+        if success:
+        
+            count, labelFileName, pathToDest = saveImages(frameId, row['fileName'], count, skip, imageRGB, imageMIE)
+            
+            detections_df = data_df.loc[data_df['fileName'] == currentVideoFileName]
+            detections_df = detections_df.loc[detections_df['frameId'] == frameId]
+            labelFile = open(pathToDest+cameraId+labelFileName, "w")
+            for i, detection in detections_df.iterrows():
+                #print(detection['fileName'], detection['x1'], detection['y1'], detection['x2'], detection['y2'])
+                w = detection['x2'] - detection['x1']
+                h = detection['y2'] - detection['y1']
+                xc = detection['x1'] + 0.5*w
+                yc = detection['y1'] + 0.5*h
+                # Label = 0 (insect)
+                line = "0 " + str(xc/IMG_WIDTH) + " " + str(yc/IMG_HEIGHT) + " " + str(w/IMG_WIDTH) + " " + str(h/IMG_HEIGHT)
+                print(line)
+                labelFile.write(line + "\n")
+            labelFile.close()
+            
+            
+if __name__=='__main__':
+    
+    TrainDataset = False
+    if TrainDataset == False: # Not used for training
+    
+        # Below should write to trainACSV2 + valACSV2
+        # Selecting videos with large insects and not detected 720p resolution
+        # numInsects = 600 # dataset_720p
+        # numUnsure = 200 # dataset_720p
+        # numVegetation = 5 # dataset_720p
+        # splitPercentage = 20 # Percentage of image used for test
+        # save_empty_frame_offset = 2
+        # pathToSrcDataset = 'E:/PAU2/detections_720p/'
+    
+        # Below should write to trainACSHQ2 + valASCHQ2
+        # Selecting videos with large insects and not detected
+        # numInsects = 80 # dataset
+        # numUnsure = 20 # dataset
+        # numVegetation = 2 # dataset
+        # save_empty_frame_offset = 2
+        # splitPercentage = 20 # Percentage of image used for test
+        # pathToSrcDataset = 'E:/PAU2/detections_FN1/'
+        
+        # Selecting videos with large insects and not detected
+        # numInsects = 40 # dataset
+        # numUnsure = 10 # dataset
+        # numVegetation = 2 # dataset    
+        # save_empty_frame_offset = 2
+        # splitPercentage = 20 # Percentage of image used for test
+        # pathToSrcDataset = 'E:/PAU2/detections_FN2/'
+        
+        # Selecting videos with false positive background detections
+        # numInsects = 100 # dataset
+        # numUnsure = 300 # dataset
+        # numVegetation = 50 # dataset
+        # save_empty_frame_offset = 0
+        # splitPercentage = 20 # Percentage of image used for test
+        # pathToSrcDataset = 'E:/PAU2/detections_FP/'
+        
+        # Below should write to testACSHQ (trainACSHQ)
+        # Selecting videos with large insects and not detected
+        numInsects = 2900 # dataset
+        numUnsure = 600 # dataset
+        numVegetation = 10 # dataset
+        save_empty_frame_offset = 0
+        splitPercentage = 100 # Percentage of image used for test
+        pathToSrcDataset = 'E:/PAU2/detections/'
+        
+
+    pathToRecordData = 'E:/PAU2/videos/'
+
+    # pathToDestDatasetMIE = 'E:/PAU2/trainACSV2m/'
+    # pathToDestDataset = 'E:/PAU2/trainACSV2/'
+    
+    # pathToDestDatasetMIE = 'E:/PAU2/trainACSHQ2m/'
+    # pathToDestDataset = 'E:/PAU2/trainACSHQ2/'
+    
+    pathToDestDatasetMIE = 'E:/PAU2/trainACSHQm/'
+    pathToDestDataset = 'E:/PAU2/trainACSHQ/'
+    
+                
+    #for filename in sorted(os.listdir(pathToRecordData)):
+    #    print(f"\"{filename}\",")
+        
+    firstTime = True
+    # File format: S2_123-Aug09_1_88-20190808104930.jpg
+    for filename in sorted(os.listdir(pathToSrcDataset)):
+        if (filename.endswith('.csv')):
+            if "-CL" in filename:
+                print("Reading", filename)
+                data_df = pd.read_csv(pathToSrcDataset+filename)
+                if firstTime:
+                    data_frames = data_df.copy()
+                    firstTime = False
+                else:    
+                    data_frames = pd.concat([data_frames, data_df])
+            
+    # Select only images where insects has been detected - many are false positive detections
+    selDataset1 = data_frames.loc[data_frames['taxaLabel'] != "Vegetation"]
+    selDataset2 = selDataset1.loc[selDataset1['taxaLabel'] != "Unsure"]
+    selDataset2 = selDataset2.sample(n=numInsects, random_state=37)
+    selDataset3 = selDataset2.sort_values(by=['fileName', 'frameId'])
+    createLabelsAndImages(selDataset3, data_frames, pathToRecordData, pathToDestDataset, pathToDestDatasetMIE, splitPercentage)
+    
+    selDataset1 = data_frames.loc[data_frames['taxaLabel'] == "Vegetation"]
+    selDataset2 = selDataset1.sample(n=numVegetation, random_state=65)
+    selDataset3 = selDataset2.sort_values(by=['fileName', 'frameId'])
+    createLabelsAndImages(selDataset3, data_frames, pathToRecordData, pathToDestDataset, pathToDestDatasetMIE, splitPercentage)
+    
+    selDataset1 = data_frames.loc[data_frames['taxaLabel'] == "Unsure"]
+    selDataset2 = selDataset1.sample(n=numUnsure, random_state=43)
+    selDataset3 = selDataset2.sort_values(by=['fileName', 'frameId'])
+    createLabelsAndImages(selDataset3, data_frames, pathToRecordData, pathToDestDataset, pathToDestDatasetMIE, splitPercentage)
+
+
+    
