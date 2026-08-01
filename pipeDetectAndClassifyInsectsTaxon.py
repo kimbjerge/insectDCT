ba3b67b22f65277d96f1a1ba46569fff83b437f8
Kim Bjerge <au288681@uni.au.dk>
2026-08-01 12:11:24 +0200

    Update pipeDetectAndClassifyInsectsTaxon.py

--- pipeDetectAndClassifyInsectsTaxon.py
+++ pipeDetectAndClassifyInsectsTaxon.py
@@ -370,7 +370,7 @@ if __name__=='__main__':
     parser.add_argument('--iou', default='0.7', type=float) # YOLO threshold for Non-Maximum Suppression (NMS). Lower values result in fewer detections by eliminating overlapping boxes, useful for reducing duplicates.
     parser.add_argument('--unsureCheck', default='Strong') # Check taxonomic consistency if confidence is above threshold (Weak) or ignore threshold (Strong)
                                                            # Strong results in more Unsure classification - default value
-                                                           # Weak allows insects to be classified to higher ranks when unsure a lower ranks with inconsistent taxonomic
+                                                           # Weak allows insects to be classified to higher ranks when unsure at lower ranks with inconsistent taxonomic
 
     args = parser.parse_args() 
     
